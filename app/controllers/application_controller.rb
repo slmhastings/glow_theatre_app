b09fa2651a58421bef9242240ff09263ec5043d5
Sequona Hastings <slmhastings@gmail.com>
2020-08-19 18:39:45 -0500

    add backend user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,30 @@ class ApplicationController < ActionController::Base
 
   def current_user
     auth_headers = request.headers["Authorization"]
-    if auth_header.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
       token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
-    begin
-      decoded_token = JWT.decode(
-      token,
-      Rails.application.credential.fetch(:secret_key_base),
-      true,
-      { algorithm: "HS256" }
-    )
-    User.find_by(id: decoded_token[0]["user_id"])
-    rescue JWT::ExpiredSignature
-      nil
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: "HS256" }
+        )
+        User.find_by(id: decoded_token[0]["user_id"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
     end
   end
+
+  helper_method :current_user
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
+ 
 end
+
+
