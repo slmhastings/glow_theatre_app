83f6fc8821fa62fd7a5fc81566f7bb9c06316784
Sequona Hastings <slmhastings@gmail.com>
2020-09-07 21:25:42 -0500

    add points to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_013807) do
+ActiveRecord::Schema.define(version: 2020_09_07_231854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_013807) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.integer "points"
   end
 
 end
